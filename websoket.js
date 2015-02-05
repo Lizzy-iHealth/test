79d88a6cd6f77c4e26987b9431fc4af23c3f71fa
lisnju <lis.ebuy@gmail.com>
2015-02-05 04:52:18 +0000

    init

--- websoket.js
+++ websoket.js
@@ -4,6 +4,7 @@ var db = new sqlite3.Database('MyTestDB.db');
 // Scream server example: "hi" -> "HI!!!" 
 var server = ws.createServer(function (conn) {
     console.log("New connection")
+    init(0,conn)
     conn.on("text", function (str) {
         console.log("Received "+str)
 	db.run("INSERT INTO content values (?)",str,function(err){})
@@ -19,4 +20,11 @@ function broadcastNewMsg(str){
     server.connections.forEach(function (connection){
 	connection.sendText(str);
 	})
+}
+
+function init(startID,conn){
+      db.each("SELECT rowid AS id, post FROM content where rowid > ?",startID, function(err, row) {
+	  conn.sendText(row.id + row.post);
+      console.log(row.id + ": " + row.post);
+  })
 }
