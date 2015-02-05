b8a5905097222158feeb86400bba67ec903b8f6f
lisnju <lis.ebuy@gmail.com>
2015-02-05 04:33:47 +0000

    broadcast

--- sql.js
+++ sql.js
@@ -5,11 +5,11 @@ var db = new sqlite3.Database('MyTestDB.db');
 db.serialize(function() {
   db.run("CREATE TABLE if not exists content(post TEXT)");
  
-  var stmt = db.prepare("INSERT INTO content valuES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("post " + i);
-  }
-  stmt.finalize();
+//  var stmt = db.prepare("INSERT INTO content valuES (?)");
+//  for (var i = 0; i < 10; i++) {
+//      stmt.run("post " + i);
+//  }
+//  stmt.finalize();
  
   db.each("SELECT rowid AS id, post FROM content", function(err, row) {
       console.log(row.id + ": " + row.post);
--- websoket.js
+++ websoket.js
@@ -1,13 +1,22 @@
 var ws = require("nodejs-websocket")
- 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('MyTestDB.db');
 // Scream server example: "hi" -> "HI!!!" 
 var server = ws.createServer(function (conn) {
     console.log("New connection")
     conn.on("text", function (str) {
         console.log("Received "+str)
-        conn.sendText(str.toUpperCase()+"!!!")
+	db.run("INSERT INTO content values (?)",str,function(err){})
+        broadcastNewMsg(str);
+	conn.sendText(str.toUpperCase()+"!!!")
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
     })
 }).listen(8001)
+
+function broadcastNewMsg(str){
+    server.connections.forEach(function (connection){
+	connection.sendText(str);
+	})
+}
