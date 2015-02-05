0bf983a7933f5dd3e598df3b8a21976bc9ed981d
lisnju <lis.ebuy@gmail.com>
2015-02-05 01:08:22 +0000

    create db

--- sql.js
+++ sql.js
@@ -1,17 +1,18 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+//var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('MyTestDB.db');
  
 db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
+  db.run("CREATE TABLE if not exists content(post TEXT)");
  
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  var stmt = db.prepare("INSERT INTO content valuES (?)");
   for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
+      stmt.run("post " + i);
   }
   stmt.finalize();
  
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
+  db.each("SELECT rowid AS id, post FROM content", function(err, row) {
+      console.log(row.id + ": " + row.post);
   });
 });
  
